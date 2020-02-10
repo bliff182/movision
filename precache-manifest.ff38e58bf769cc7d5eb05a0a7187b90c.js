3a3683f4404d99e6c64bc408bfc00cd92bf7ca29
bliff182 <michael.bither@gmail.com>
2020-02-10 10:47:44 -0500

    Updates

---
+++ precache-manifest.ff38e58bf769cc7d5eb05a0a7187b90c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567a846d593858750fa9ee8a95861937",
+    "url": "/movision/index.html"
+  },
+  {
+    "revision": "8a315794352cb95a9b6f",
+    "url": "/movision/static/css/main.59740667.chunk.css"
+  },
+  {
+    "revision": "19d970e8e99b82bd7c85",
+    "url": "/movision/static/js/2.c2105e80.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movision/static/js/2.c2105e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a315794352cb95a9b6f",
+    "url": "/movision/static/js/main.ce87399f.chunk.js"
+  },
+  {
+    "revision": "1b3627b5798f03c074cd",
+    "url": "/movision/static/js/runtime-main.7ff9204d.js"
+  }
+]);
